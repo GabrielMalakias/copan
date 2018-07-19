9873dc29e9376d4e8c1d441e09adff26d28878e2
Gabriel Malaquias de Souza <gabriel.malaquias@shore.com>
2018-07-19 09:14:15 +0200

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,10 +2,15 @@ Copan.Repo.all(Copan.Schema.User)
 |> Enum.map(fn(user) -> Copan.Commands.User.Destroy.call(user) end)
 
 
-Copan.Commands.User.Create.call(%{name: "Gabriel", email: "gabriel07malakias@gmail.com"})
+user_params = %{
+  name: "Copan",
+  email: "copan@copan.com",
+  reference_id: "00000000-0000-4000-8000-000000000000"
+}
 
+Copan.Commands.User.Create.call(user_params)
 
-user = Copan.Repo.one!(Copan.Schema.User, email: "gabriel07malakias@gmail.com")
+user = Copan.Repo.one!(Copan.Schema.User, email: "copan@copan.com")
 
 category = Copan.Commands.Category.Create.call(user, %{name: "Auto", position: 2})
 
@@ -14,7 +19,25 @@ title = "Das neue Auto"
 description = "Das neue Auto"
 content = "Das neue Auto"
 
-Copan.Commands.Article.Create.call(user, category, %{title: title,
+article_params = %{
+  title: title,
   description: description,
   content: content,
-  image: image})
+  image: image
+}
+Copan.Commands.Article.Create.call(user, category, article_params)
+
+
+Enum.map(1..300, fn (x) ->
+  no_show = rem(x, 2) == 0
+  status = if rem(x, 2) == 0, do: "created", else: "confirmed"
+
+  params = %{
+    user_id: user.reference_id,
+    status: status,
+    no_show: no_show,
+    reference_id: Ecto.UUID.generate,
+  }
+
+  Copan.Commands.Appointment.Create.call(params)
+end)
